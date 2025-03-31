71194c84c083933d966926101722f18faa4fc154
bloshko <mis.bloko@gmail.com>
2025-03-31 15:00:34 +0200

    Change dev server port to 4000

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
 // Kommenter ut linjen ved å fjerne // og bytt ut DITT_TEMA med navnet på temaet ditt
-// import '../design-tokens-build/DITT_TEMA.css'
-import { Button, Heading, Paragraph } from '@digdir/designsystemet-react';
-import './App.css'
+import "../design-tokens-build/theme.css";
+import {
+  Button,
+  Heading,
+  Paragraph,
+  Textfield,
+} from "@digdir/designsystemet-react";
+import "./App.css";
 
 function App() {
   return (
     <>
-      <Heading data-size='lg' level={1}>Velkommen til Designsystemet workshop.</Heading>
-      <Paragraph>Redigere kildekoden i <code>App.tsx</code> for å endre utseendet på denne siden</Paragraph>
-      <Button >Knapp</Button>
+      <Heading data-size="lg" level={1}>
+        Velkommen til Designsystemet workshop.
+      </Heading>
+      <Paragraph>
+        Redigere kildekoden i <code>App.tsx</code> for å endre utseendet på
+        denne siden
+      </Paragraph>
+      <Button>Knapp</Button>
+      {/* <Textfield label="My label">Test text field</Textfield> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
